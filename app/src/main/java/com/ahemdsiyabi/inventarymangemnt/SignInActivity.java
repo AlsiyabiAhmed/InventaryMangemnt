4fef3f2494f641096d26f8bd16bc8528662b94cc
AlineJo <josephali1993@gmail.com>
2023-01-10 21:57:03 +0400

    singup and singin with fb done

--- app/src/main/java/com/ahemdsiyabi/inventarymangemnt/SignInActivity.java
+++ app/src/main/java/com/ahemdsiyabi/inventarymangemnt/SignInActivity.java
@@ -2,13 +2,99 @@ package com.ahemdsiyabi.inventarymangemnt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+
+
+    private EditText inputEmail;
+    private EditText inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+
+        Button buttonSignIn = findViewById(R.id.buttonSignIn);
+        buttonSignIn.setOnClickListener(v -> {
+            checkInputsData();
+        });
+
+        Button buttonSignUp = findViewById(R.id.buttonSignUp);
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToSignUpActivity();
+            }
+        });
+    }
+
+    private void checkInputsData() {
+        String email = inputEmail.getText().toString();
+        String pass = inputPassword.getText().toString();
+
+        //check email Pattern
+        String matchPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (email.length() == 0) {
+            Toast.makeText(SignInActivity.this, "enter your email", Toast.LENGTH_SHORT).show();
+        } else if (!email.matches(matchPattern)) {
+            Toast.makeText(SignInActivity.this, "email not valid!", Toast.LENGTH_SHORT).show();
+        } else if (pass.length() == 0) {
+            Toast.makeText(SignInActivity.this, "enter your password", Toast.LENGTH_SHORT).show();
+        } else if (pass.length() < 6) {
+            Toast.makeText(SignInActivity.this, "password length less than 6 characters!", Toast.LENGTH_SHORT).show();
+        } else {
+            signInByFB(email, pass);
+        }
+    }
+
+    private void signInByFB(String email, String pass) {
+        firebaseAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, task -> {
+
+                    if (task.isSuccessful()) {
+                        // Sign in success
+                        firebaseUser = firebaseAuth.getCurrentUser();
+                        moveToMainActivity();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("SignInActivity", "signIn:failed", task.getException());
+                        Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+
+    private void moveToMainActivity() {
+        Intent i = new Intent(SignInActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void moveToSignUpActivity() {
+        Intent i = new Intent(SignInActivity.this, SignInActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/ahemdsiyabi/inventarymangemnt/SignUpActivity.java
+++ app/src/main/java/com/ahemdsiyabi/inventarymangemnt/SignUpActivity.java
@@ -2,13 +2,145 @@ package com.ahemdsiyabi.inventarymangemnt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.ahemdsiyabi.inventarymangemnt.mypackage.FBConstants;
+import com.ahemdsiyabi.inventarymangemnt.mypackage.IMUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+
+
+    private EditText inputPersonName;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private EditText inputRewritePassword;
+
+    // on default back button clicked close this screen
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // display default back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        inputPersonName = findViewById(R.id.inputPersonName);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputRewritePassword = findViewById(R.id.inputRewritePassword);
+
+        Button buttonSignUp = findViewById(R.id.buttonSignUp);
+        buttonSignUp.setOnClickListener(v -> {
+            checkInputsData();
+        });
+
+
     }
+
+
+    private void checkInputsData() {
+        String pName = inputPersonName.getText().toString();
+        String email = inputEmail.getText().toString();
+        String pass = inputPassword.getText().toString();
+        String rePass = inputRewritePassword.getText().toString();
+
+        //check email Pattern
+        String matchPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (pName.length() == 0) {
+            Toast.makeText(SignUpActivity.this, "enter your name", Toast.LENGTH_SHORT).show();
+        } else if (email.length() == 0) {
+            Toast.makeText(SignUpActivity.this, "enter your email", Toast.LENGTH_SHORT).show();
+        } else if (!email.matches(matchPattern)) {
+            Toast.makeText(SignUpActivity.this, "email not valid!", Toast.LENGTH_SHORT).show();
+        } else if (pass.length() == 0) {
+            Toast.makeText(SignUpActivity.this, "enter your password", Toast.LENGTH_SHORT).show();
+        } else if (pass.length() < 6) {
+            Toast.makeText(SignUpActivity.this, "password length less than 6 characters!", Toast.LENGTH_SHORT).show();
+        } else if (rePass.length() == 0) {
+            Toast.makeText(SignUpActivity.this, "rewrite password is empty!", Toast.LENGTH_SHORT).show();
+        } else if (rePass.length() < 6) {
+            Toast.makeText(SignUpActivity.this, "rewrite password length less than 6 characters!", Toast.LENGTH_SHORT).show();
+        } else if (!rePass.equals(pass)) {
+            Toast.makeText(SignUpActivity.this, "password not equal to rewrite password", Toast.LENGTH_SHORT).show();
+        } else {
+            signUpToFB(pName, email, pass);
+        }
+    }
+
+    private void signUpToFB(String pName, String email, String pass) {
+
+        firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success
+                        firebaseUser = firebaseAuth.getCurrentUser();
+                        createNewFBUser(pName, email);
+                    } else {
+                        // If sign in fails
+                        Log.d("SignUpActivity", "Sign in:failed", task.getException());
+                        Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+    }
+
+    private void createNewFBUser(String pName, String email) {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(FBConstants.FB_KEY_USERS);
+
+        IMUser u = new IMUser();
+        u.setUserId(firebaseUser.getUid());
+        u.setUserPersonName(pName);
+        u.setUserEmail(email);
+        Log.d("SingUpActictity", "createNewFBUser: " + u.getUserId());
+
+        myRef.child(u.getUserId()).setValue(u).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                // Sign in success, 
+                moveToMainActivity();
+
+            } else {
+                // If sign in fails
+                Log.d("SignUpActivity", "Signup:failure", task.getException());
+                Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private void moveToMainActivity() {
+        Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+
 }
