2f5dfc709f66c6eebdf03d8712f3acb2276c855e
AlineJo <josephali1993@gmail.com>
2023-01-10 21:54:54 +0400

    create model classes

---
+++ app/src/main/java/com/ahemdsiyabi/inventarymangemnt/mypackage/FBConstants.java
@@ -0,0 +1,5 @@
+package com.ahemdsiyabi.inventarymangemnt.mypackage;
+
+public class FBConstants {
+    public static final String FB_KEY_USERS = "fb_users";
+}
---
+++ app/src/main/java/com/ahemdsiyabi/inventarymangemnt/mypackage/IMUser.java
@@ -0,0 +1,40 @@
+package com.ahemdsiyabi.inventarymangemnt.mypackage;
+
+public class IMUser {
+    private String userId;
+    private String userPersonName;
+    private String userEmail;
+    private String userPassword;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPersonName() {
+        return userPersonName;
+    }
+
+    public void setUserPersonName(String userPersonName) {
+        this.userPersonName = userPersonName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
